d7fac74ea62638f779f3d86f8d15742d6b9ad255
klmwook <klmwook72@gmail.com>
2023-06-26 13:15:13 +0900

    기본 header link 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
+import { Route } from 'react-router-dom';
+
+//Common
 import Footer from './components/common/Footer';
 import Header from './components/common/Header';
+
+//Main
 import Visual from './components/main/Visual';
+
+//Sub
 import Community from './components/sub/Community';
 import Contact from './components/sub/Contact';
 import Department from './components/sub/Department';
 import Gallery from './components/sub/Gallery';
 import Member from './components/sub/Member';
 import Youtube from './components/sub/Youtube';
+import News from './components/main/News';
+import Pics from './components/main/Pics';
+import Vids from './components/main/Vids';
+import Banner from './components/main/Banner';
 
 function App() {
 	return (
 		<>
 			<Header />
 
-			<Visual />
-			<Department />
-			<Community />
-			<Gallery />
-			<Youtube />
-			<Contact />
-			<Member />
+			<Route exact path='/'>
+				<Visual />
+				<News />
+				<Pics />
+				<Vids />
+				<Banner />
+			</Route>
+
+			<Route path='/department'>
+				<Department />
+			</Route>
+
+			<Route path='/community'>
+				<Community />
+			</Route>
+
+			<Route path='/gallery'>
+				<Gallery />
+			</Route>
+
+			<Route path='/youtube'>
+				<Youtube />
+			</Route>
+
+			<Route path='/contact'>
+				<Contact />
+			</Route>
+
+			<Route path='/member'>
+				<Member />
+			</Route>
 
 			<Footer />
 		</>
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,36 @@
+//Link - 기본 링크 기능
+//NavLink - 기본 링크 기능 + 활성화 기능
+import { Link, NavLink } from 'react-router-dom';
+
 function Header() {
-	return <header>Header</header>;
+	return (
+		<header>
+			<h1>
+				<Link to='/'>LOGO</Link>
+			</h1>
+
+			<ul id='gnb'>
+				<li>
+					<NavLink to='/department'>Department</NavLink>
+				</li>
+				<li>
+					<NavLink to='/community'>Community</NavLink>
+				</li>
+				<li>
+					<NavLink to='/gallery'>Gallery</NavLink>
+				</li>
+				<li>
+					<NavLink to='/youtube'>Youtube</NavLink>
+				</li>
+				<li>
+					<NavLink to='/contact'>Contact</NavLink>
+				</li>
+				<li>
+					<NavLink to='/member'>Member</NavLink>
+				</li>
+			</ul>
+		</header>
+	);
 }
 
 export default Header;

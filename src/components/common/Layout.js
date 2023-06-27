142ce027c74e765182f98bac660e45282023e010
klmwook <klmwook72@gmail.com>
2023-06-27 10:50:42 +0900

    Layout 추가

---
+++ src/components/common/Layout.js
@@ -0,0 +1,13 @@
+function Layout({ name }) {
+	return (
+		<section className={`content ${name}`}>
+			<figure></figure>
+
+			<div className='inner'>
+				<h1>{name}</h1>
+			</div>
+		</section>
+	);
+}
+
+export default Layout;

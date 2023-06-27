d8c70acb0de15e961be86fd8411ba50556836e00
klmwook <klmwook72@gmail.com>
2023-06-27 11:40:35 +0900

    sub layout 반응형 추가

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,10 +1,20 @@
-function Layout({ name }) {
+import { useEffect, useRef } from 'react';
+
+function Layout({ name, children }) {
+	const frame = useRef(null);
+
+	//Mount가 되는 순간 한번만 실행 되는 구문
+	useEffect(() => {
+		frame.current.classList.add('on');
+	}, []);
+
 	return (
-		<section className={`content ${name}`}>
+		<section className={`content ${name}`} ref={frame}>
 			<figure></figure>
 
 			<div className='inner'>
 				<h1>{name}</h1>
+				{children}
 			</div>
 		</section>
 	);
--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,7 +1,11 @@
 import Layout from '../common/Layout';
 
 function Department() {
-	return <Layout name={'Department'}></Layout>;
+	return (
+		<Layout name={'Department'}>
+			<p>회사 조직소개 페이지 입니다.</p>
+		</Layout>
+	);
 }
 
 export default Department;

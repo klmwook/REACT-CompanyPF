e7c5fd81f1fdd5fb2933ecfb827e71f8bbaeeced
klmwook <klmwook72@gmail.com>
2023-06-27 12:47:46 +0900

    Department subPage 추가

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
 import Layout from '../common/Layout';
+import axios from 'axios';
 
 function Department() {
+	const [Members, setMembers] = useState([]);
+	console.log(Members);
+
+	//최초의 데이터를 가져와야 되기 때문에 useEffect를 사용
+	useEffect(() => {
+		axios.get(`${process.env.PUBLIC_URL}/DB/members.json`).then((data) => {
+			console.log(data);
+			setMembers(data.data.members);
+		});
+	}, []);
+
 	return (
 		<Layout name={'Department'}>
-			<p>회사 조직소개 페이지 입니다.</p>
+			{Members.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<div className='pic'>
+							<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+							<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+						</div>
+						<h2>{member.name}</h2>
+						<p>{member.position}</p>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }

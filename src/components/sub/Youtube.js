4c67fa3b51423f8a713941f3a939a3db93953fea
klmwook <klmwook72@gmail.com>
2023-06-27 10:58:38 +0900

    공통 sub layout 스타일링

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,7 +1,7 @@
 import Layout from '../common/Layout';
 
 function Youtube() {
-	return <Layout name={'Youtube'}></Layout>;
+	return <Layout name={'Youtube'}>유튜브 컨텐츠</Layout>;
 }
 
 export default Youtube;

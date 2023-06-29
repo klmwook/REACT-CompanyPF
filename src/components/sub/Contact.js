cf358c265ca360a1c45970e0d1a517140030e693
klmwook <klmwook72@gmail.com>
2023-06-29 13:20:19 +0900

    kakao map 추가 및 toggle 정보로 교통정보 on/off 기능 추가

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -1,7 +1,9 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Layout from '../common/Layout';
 
 function Contact() {
+	const [Traffic, setTraffic] = useState(false);
+
 	//지도가 들어갈 프레임도 가상요소 참조를 위해 useRef로 참조 객체 생성
 	const container = useRef(null);
 	//일반 HTML 버전과는 달리 윈도우 객체에서 직접 Kakao 상위 객체값을 뽑아옴
@@ -13,13 +15,28 @@ function Contact() {
 
 	//카카오 맵 연결 해보기~
 	useEffect(() => {
+		const mapInstance = new kakao.maps.Map(container.current, option);
+
 		//인스턴스 호출 구문은 Component Mount 이후에 호출
-		new kakao.maps.Map(container.current, option);
-	});
+		const imgSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
+		const imgSize = new kakao.maps.Size(232, 99);
+		const imgOption = { offset: new kakao.maps.Point(116, 99) };
+		const markerImg = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOption);
+
+		const marker = new kakao.maps.Marker({
+			position: option.center,
+			image: markerImg,
+		});
+
+		Traffic ? mapInstance.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : mapInstance.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+
+		marker.setMap(mapInstance);
+	}, [Traffic]);
 
 	return (
 		<Layout name={'Contact'}>
 			<div id='map' ref={container}></div>
+			<button onClick={() => setTraffic(!Traffic)}>{Traffic ? '교통정보 off' : '교통정보 on'}</button>
 		</Layout>
 	);
 }

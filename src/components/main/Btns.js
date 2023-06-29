18b74e418a43c299a179b2b4d3f7180e41139398
klmwook <klmwook72@gmail.com>
2023-06-29 09:39:01 +0900

    main page scroll 기능 추가

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -15,15 +15,29 @@ function Btns() {
 		setNum(pos.current.length); //현재 section 값 찾음
 	};
 
+	const activation = () => {
+		const scroll = window.scrollY;
+		const btns = btnRef.current.children;
+
+		pos.current.forEach((pos, idx) => {
+			if (scroll >= pos) {
+				for (const btn of btns) btn.classList.remove('on');
+				btns[idx].classList.add('on');
+			}
+		});
+	};
+
 	useEffect(() => {
 		getPos();
 		window.addEventListener('resize', getPos);
+		window.addEventListener('scroll', activation);
 
 		return () => {
 			//윈도우 객체에 이벤트 연결하면 다른 서브페이지의 컴포넌트에서도 동일하게 함수호출되므로 에러 발생
 			//해당 컴포넌트가 unmount시 무조건 window전역객체에 연결되어 있는 이벤트 핸들러 함수 제거
 			//이떄 removeEventListener로 핸들러 함수를 제거하기 위해서는 해당 함수로 외부로 함수로 선언되어 있어야가능
 			window.removeEventListener('resize', getPos);
+			window.removeEventListener('scroll', activation);
 		};
 	}, []);
 
@@ -32,6 +46,11 @@ function Btns() {
 			{Array(Num)
 				.fill()
 				.map((_, idx) => {
+					let defalutClass = '';
+					if (idx === 0) {
+						defalutClass = 'on';
+					}
+
 					return (
 						<li
 							key={idx}
@@ -42,6 +61,7 @@ function Btns() {
 									duration: 500,
 								});
 							}}
+							className={defalutClass}
 						></li>
 					);
 				})}

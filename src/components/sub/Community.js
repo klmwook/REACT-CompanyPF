eea4f775f7ecde09b60d50f3e0ed3e647f6065c5
klmwook <klmwook72@gmail.com>
2023-07-04 12:20:53 +0900

    Community 기능 추가

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,7 +1,127 @@
 import Layout from '../common/Layout';
+import { useRef, useState, useEffect } from 'react';
 
 function Community() {
-	return <Layout name={'Community'}></Layout>;
+	const input = useRef(null);
+	const textarea = useRef(null);
+	const [Posts, setPosts] = useState([]);
+	const [Allowed, setAllowed] = useState(true);
+
+	const resetForm = () => {
+		input.current.value = '';
+		textarea.current.value = '';
+	};
+
+	const createPost = () => {
+		if (!input.current.value.trim() || !textarea.current.value.trim()) {
+			resetForm();
+			return alert('제목과 본문을 모두 입력하세요.');
+		}
+		setPosts([...Posts, { title: input.current.value, content: textarea.current.value }]);
+		resetForm();
+	};
+
+	const deletePost = (delIndex) => {
+		if (window.confirm('정말로 삭제 하시겠습니까?')) {
+			setPosts(Posts.filter((_, idx) => idx !== delIndex));
+		}
+	};
+
+	const enableUpdate = (editIndex) => {
+		//수정 모드 진입 함수 호출시 Allowed가 true 일때에만 로직이 실행되도록 처리
+		if (!Allowed) {
+			return;
+		}
+		//일단 로직이 실행되면 Allowed 값을 false로 바꿔서 이후부터는 다시 수정모드로 진입되는 것을 방지
+		setAllowed(false);
+		setPosts(
+			Posts.map((post, postIndex) => {
+				if (editIndex === postIndex) post.enableUpdate = true;
+				return post;
+			})
+		);
+	};
+
+	const disableUpdate = (editIndex) => {
+		setPosts(
+			Posts.map((post, postIndex) => {
+				if (editIndex === postIndex) post.enableUpdate = false;
+				return post;
+			})
+		);
+		//글 수정 취소버튼을 눌러서 disableUpdate 함수가 호출이 되야지만 Allowed값을 다시 true로 바꿔서 글 수정 가능하게 처리
+		setAllowed(true);
+	};
+
+	useEffect(() => {
+		console.log(Posts);
+	}, [Posts]);
+
+	return (
+		<Layout name={'Community'}>
+			<div className='inputBox'>
+				<input type='text' placeholder='제목을 입력하세요.' ref={input} /> <br />
+				<textarea cols='30' rows='3' placeholder='본문을 입력하세요.' ref={textarea}></textarea> <br />
+				<nav className='btnSet'>
+					<button onClick={resetForm}>cancel</button>
+					<button onClick={createPost}>write</button>
+				</nav>
+			</div>
+
+			<div className='showBox'>
+				{Posts.map((post, idx) => {
+					return (
+						<article key={idx}>
+							{post.enableUpdate ? (
+								//수정모드
+								<>
+									<div className='txt'>
+										<input type='text' defaultValue={post.title} />
+										<br />
+										<textarea cols='30' rows='3' defaultValue={post.content}></textarea>
+									</div>
+
+									<nav className='btnSet'>
+										<button onClick={() => disableUpdate(idx)}>CANCEL</button>
+										<button>UPDATE</button>
+									</nav>
+								</>
+							) : (
+								//출력모드
+								<>
+									<div className='txt'>
+										<h2>{post.title}</h2>
+										<p>{post.content}</p>
+									</div>
+
+									<nav className='btnSet'>
+										<button onClick={() => enableUpdate(idx)}>EDIT</button>
+										<button onClick={() => deletePost(idx)}>DELETE</button>
+									</nav>
+								</>
+							)}
+						</article>
+					);
+				})}
+			</div>
+		</Layout>
+	);
 }
 
 export default Community;
+
+/* 
+CRUD
+Create - 데이터 저장 (게시글 저장)
+Read - 데이터 호출 (게시글 보기)
+Update - 데이터 수정 (게시글 수정)
+Delete - 데이터 삭제 (게시글 삭제)
+
+localStorage : 모든 브라우저마다 가지고 있는 경량의 데이터 베이스 (문자열 저장)
+
+수정 모드 작업 흐름
+1- 수정 버튼 클릭시 해당 순번의 Posts의 객체에 수정관련 property 추가
+2- map으로 반복처리시 수정관련 property의 유무에 따라 수정모드, 출력모드 구분해서 분기처리 후 렌더링
+3- 출력모드: h2, p로 출력 / 수정모드: input, textarea로 값을 담아서 출력 (수정취소, 수정 버튼 추가)
+4- 수정모드에서 수정버튼 클릭시 State값 변경하고 해당 포스트의 수정관련 property 수정
+*/
--- src/components/sub/Member.js
+++ src/components/sub/Member.js
@@ -1,27 +1,69 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Layout from '../common/Layout';
+import { useHistory } from 'react-router-dom';
 
 function Member() {
+	const selectEl = useRef(null);
+	const radioGroup = useRef(null);
+	const checkGroup = useRef(null);
+	const history = useHistory();
 	const initVal = {
 		userid: '',
 		pwd1: '',
 		pwd2: '',
 		email: '',
+		gender: false,
+		interests: false,
+		edu: null,
+		comments: '',
 	};
 
 	const [Val, setVal] = useState(initVal);
+	const [Err, setErr] = useState({});
+	const [Submit, setSubmit] = useState(false);
 
-	//현재 입력하고 있는 input 요소의 name, value 값을 비구조화할당으로 뽑아서 출력
 	const handleChange = (e) => {
+		//현재 입력하고 있는 input요소의 name,value값을 비구조화할당으로 뽑아서 출력
 		const { name, value } = e.target;
-		//기존 초기 Val State 값을 deep copy해서 현재 입력하고 있는 항목의 name값과 value값으로 기존 State를 덮어쓰기 해서 변경 (불변성 유지)
+		//기존 초기 Val State값을 deep copy해서 현재 입력하고 있는 항목의 name값과 value값으로 기존 State를 덮어쓰기 해서 변경 (불변성 유지)
 		setVal({ ...Val, [name]: value });
 	};
 
+	const handleRadio = (e) => {
+		const { name, checked } = e.target;
+		console.log(e.target.value);
+		setVal({ ...Val, [name]: checked });
+	};
+
+	const handleCheck = (e) => {
+		const { name } = e.target;
+		let isChecked = false;
+		const inputs = e.target.parentElement.querySelectorAll('input');
+
+		//모든 체크박스를 반복돌면서 하나라도 체크되어 있는게 있으면 true값 반환
+		inputs.forEach((el) => el.checked && (isChecked = true));
+		setVal({ ...Val, [name]: isChecked });
+	};
+
+	const handleSelect = (e) => {
+		const { name, value } = e.target;
+		setVal({ ...Val, [name]: value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('현재 스테이트값', Val);
+		//check가 반환하는 인증 메세지가 있으면 해당 메세지를 화면에 출력하고 전송중지
+		//그렇지 않으면 인증 성공
+		console.log(check(Val));
+		setErr(check(Val));
+		setSubmit(true);
+	};
+
 	const check = (value) => {
-		//인수로 현재 State 값을 전달 받아서 항목별로 에러메세지를 객체로 반환하는 함수
-		//반환되는 에러메시지가 있으면 인증 실패
-		//반환되는 에러메시지가 없으면 인증 성공
+		//인수로 현재 State값을 전달받아서 항목별로 에러메세지를 객체로 반환하는 함수
+		//반환되는 에러메세지가 있으면 인증 실패
+		//반환되는 에러메세지가 없으면 인증 성공
 		const errs = {};
 		const eng = /[a-zA-Z]/;
 		const num = /[0-9]/;
@@ -30,33 +72,55 @@ function Member() {
 		if (value.userid.length < 5) {
 			errs.userid = '아이디를 5글자 이상 입력하세요.';
 		}
-		if (value.pwd1.length < 5 || !eng.test(value.pwd1) || !num.text(value.pwd1) || !spc.test(value.pwd1)) {
+		if (value.pwd1.length < 5 || !eng.test(value.pwd1) || !num.test(value.pwd1) || !spc.test(value.pwd1)) {
 			errs.pwd1 = '비밀번호는 5글자 이상, 영문, 숫자, 특수문자를 모두 포함하세요.';
 		}
 		if (value.pwd1 !== value.pwd2 || !value.pwd2) {
-			errs.pwd2 = '두개의 비밀번호를 동일하게 입력 하세요.';
+			errs.pwd2 = '두개의 비밀번호를 동일하게 입력하세요.';
 		}
 		if (value.email.length < 8 || !/@/.test(value.email)) {
-			errs.email = '이메일 주소는 8글자 이상 @를 포함하세요.';
+			errs.email = '이메일주소는 8글자 이상 @를 포함하세요.';
 		}
-
+		if (!value.gender) {
+			errs.gender = '성별을 체크해주세요.';
+		}
+		if (!value.interests) {
+			errs.interests = '관심사를 하나 이상 체크하세요.';
+		}
+		if (value.edu === '') {
+			errs.edu = '최종학력을 선택하세요.';
+		}
+		if (value.comments.length < 10) {
+			errs.comments = '남기는 말을 최소 10글자 이상 입력하세요.';
+		}
 		return errs;
 	};
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log('현재 State 값', Val);
-		//check가 반호나하는 인증 메세지가 있으면 해당 메시지를 화면에 출력하고 전송 중지
-		//그렇지 않으면 인증 성공
-		console.log(check(Val));
+	const resetForm = () => {
+		const select = selectEl.current.options[0];
+		const checks = checkGroup.current.querySelectorAll('input');
+		const radios = radioGroup.current.querySelectorAll('input');
+		select.selected = true;
+		checks.forEach((el) => (el.checked = false));
+		radios.forEach((el) => (el.checked = false));
+		setVal(initVal);
 	};
 
 	useEffect(() => {
-		console.log(Val);
-	}, [Val]);
+		//객체의 키값을 배열로 반환한다음 해당 배열의 갯수를 저장
+		//len값이 0이면 Err객체에 에러메시지가 하나도 없어서 인증통과 처리
+		const len = Object.keys(Err).length;
+		if (len === 0 && Submit) {
+			alert('모든 인증을 통과했습니다.');
+			//history.push('/');
+			resetForm();
+		}
+	}, [Err]);
 
 	return (
 		<Layout name={'Member'}>
+			<button onClick={() => history.goBack()}>뒤로 가기</button>
+
 			<form onSubmit={handleSubmit}>
 				<fieldset>
 					<legend className='h'>회원가입 폼 양식</legend>
@@ -68,7 +132,9 @@ function Member() {
 									<label htmlFor='userid'>USER ID</label>
 								</th>
 								<td>
-									<input type='text' name='userid' id='userid' placeholder='아이디를  입력하세요.' onChange={handleChange} />
+									<input type='text' name='userid' id='userid' placeholder='아이디를 입력하세요' onChange={handleChange} value={Val.userid} />
+									<br />
+									{Err.userid && <p>{Err.userid}</p>}
 								</td>
 							</tr>
 
@@ -77,8 +143,11 @@ function Member() {
 								<th>
 									<label htmlFor='pwd1'>PASSWORD</label>
 								</th>
+
 								<td>
-									<input type='password' name='pwd1' id='pwd1' placeholder='비밀번호를 입력하세요' onChange={handleChange} />
+									<input type='password' name='pwd1' id='pwd1' placeholder='비밀번호를 입력하세요' onChange={handleChange} value={Val.pwd1} />
+									<br />
+									{Err.pwd1 && <p>{Err.pwd1}</p>}
 								</td>
 							</tr>
 
@@ -88,24 +157,88 @@ function Member() {
 									<label htmlFor='pwd2'>RE-PASSWORD</label>
 								</th>
 								<td>
-									<input type='password' name='pwd2' id='pwd2' placeholder='비밀번호를 재 입력하세요' onChange={handleChange} />
+									<input type='password' name='pwd2' id='pwd2' placeholder='비밀번호를 재입력하세요' onChange={handleChange} value={Val.pwd2} />
+									<br />
+									{Err.pwd2 && <p>{Err.pwd2}</p>}
 								</td>
 							</tr>
 
-							{/* e mail */}
+							{/* e-mail */}
 							<tr>
 								<th>
 									<label htmlFor='email'>E-MAIL</label>
 								</th>
 								<td>
-									<input type='text' name='email' id='email' placeholder='이메일 주소를 입력하세요' onChange={handleChange} />
+									<input type='text' name='email' id='email' placeholder='이메일주소를 입력하세요' onChange={handleChange} value={Val.email} />
+									<br />
+									{Err.email && <p>{Err.email}</p>}
+								</td>
+							</tr>
+
+							{/* gender */}
+							<tr>
+								<th>GENDER</th>
+								<td ref={radioGroup}>
+									<label htmlFor='male'>Male</label>
+									<input type='radio' name='gender' value='male' id='mail' onChange={handleRadio} />
+
+									<label htmlFor='female'>FeMale</label>
+									<input type='radio' name='gender' value='female' id='female' onChange={handleRadio} />
+									<br />
+									{Err.gender && <p>{Err.gender}</p>}
+								</td>
+							</tr>
+
+							{/* interest */}
+							<tr>
+								<th>INTERESTS</th>
+								<td ref={checkGroup}>
+									<label htmlFor='music'>Music</label>
+									<input type='checkbox' name='interests' value='music' id='music' onChange={handleCheck} />
+
+									<label htmlFor='reading'>Reading</label>
+									<input type='checkbox' name='interests' value='reading' id='reading' onChange={handleCheck} />
+
+									<label htmlFor='game'>Game</label>
+									<input type='checkbox' name='interests' value='game' id='game' onChange={handleCheck} />
+									<br />
+									{Err.interests && <p>{Err.interests}</p>}
+								</td>
+							</tr>
+
+							{/* education */}
+							<tr>
+								<th>
+									<label htmlFor='edu'>EDUCATION</label>
+								</th>
+								<td>
+									<select name='edu' id='edu' onChange={handleSelect} ref={selectEl}>
+										<option value=''>최종학력을 선택하세요</option>
+										<option value='elementary-school'>초등학교 졸업</option>
+										<option value='middle-school'>중학교 졸업</option>
+										<option value='high-school'>고등학교 졸업</option>
+										<option value='college'>대학교 졸업</option>
+									</select>
+									{Err.edu && <p>{Err.edu}</p>}
+								</td>
+							</tr>
+
+							{/* comments */}
+							<tr>
+								<th>
+									<label htmlFor='comments'>Leave Message</label>
+								</th>
+								<td>
+									<textarea name='comments' id='comments' cols='30' rows='3' value={Val.comments} onChange={handleChange}></textarea>
+									<br />
+									{Err.comments && <p>{Err.comments}</p>}
 								</td>
 							</tr>
 
 							{/* btn set */}
 							<tr>
 								<th colSpan='2'>
-									<input type='reset' value='CANCEL' />
+									<input type='reset' value='CANCEL' onClick={() => setVal(initVal)} />
 									<input type='submit' value='SEND' />
 								</th>
 							</tr>

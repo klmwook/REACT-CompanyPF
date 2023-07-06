489e99493979e4c2399c64433f1946be4aa2cf9e
klmwook <klmwook72@gmail.com>
2023-07-06 12:14:30 +0900

    gallery 기능 추가 및 수정

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,60 +1,127 @@
-import { useState, useEffect, useRef } from 'react';
-import Masonry from 'react-masonry-component';
 import Layout from '../common/Layout';
+import Masonry from 'react-masonry-component';
 import axios from 'axios';
+import { useState, useEffect, useRef } from 'react';
 
 function Gallery() {
+	const btnSet = useRef(null);
+	const enableEvent = useRef(true);
+	const searchInput = useRef(null);
 	const frame = useRef(null);
-	const counter = useRef(0);
 	const [Items, setItems] = useState([]);
 	const [Loader, setLoader] = useState(true);
 
 	const getFlickr = async (opt) => {
-		const baseURL = `https://www.flickr.com/services/rest/?format=json&nojsoncallback=1`;
+		console.log('getFlickr');
+		let counter = 0;
+		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
 		const key = '4b95b58f2acca136d03e1c6883048c6c';
-		const method_interest = 'flickr.interestingness.getList'; //오늘의 인기있는 이미지
+		const method_interest = 'flickr.interestingness.getList';
+		const method_user = 'flickr.people.getPhotos';
 		const method_search = 'flickr.photos.search';
-		const num = 500;
-
+		const num = 50;
 		let url = '';
+
 		if (opt.type === 'interest') url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
 		if (opt.type === 'search') url = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.tags}`;
-		if (opt.type === 'user') url = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&user_id=${opt.user}`;
+		if (opt.type === 'user') url = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.user}`;
 
 		const result = await axios.get(url);
+		if (result.data.photos.photo.length === 0) {
+			setLoader(false);
+			frame.current.classList.add('on');
+			return alert('이미지가 없습니다.');
+		}
+
 		setItems(result.data.photos.photo);
 
-		//외부데이터가 State에 담기고 DOM이 생성되는 순간
-		//모든 img 요소를 찾아서 반복처리
 		const imgs = frame.current.querySelectorAll('img');
-		imgs.forEach((img, idx) => {
-			//이미지 요소에 load 이벤트가 발생할 때 (소스 이미지까지 로딩이 완료 될 때 마다)
+		imgs.forEach((img) => {
 			img.onload = () => {
-				//내부적으로 카운터 값을 1씩 증가
-				++counter.current;
+				++counter;
+				console.log(counter);
 
-				//로딩 완료된 이미지 수와 전체 이미지 수가 같아지면
-				if (counter.current === imgs.length) {
-					//loader 제거하고 이미지 갤러리 보임 처리
+				//임시방편 - 전체 이미지 갯수가 하나 모잘라도 출력되게 수정
+				//문제점 - myGallery, interestGallery는 전체 이미지 카운트가 잘 되는데 특정 사용자 갤러리만 갯수가 1씩 모자라는 현상
+				if (counter === imgs.length - 1) {
 					setLoader(false);
 					frame.current.classList.add('on');
+
+					//모션중 재이벤트 방지시 모션이 끝날때까지 이벤트를 방지를 시켜도
+					//모션이 끝나는순간에도 이벤트가 많이 발생하면 특정값이 바뀌는 순간보다 이벤트가 더 빨리들어가서 오류가 발생가능
+					//해결방법 - 물리적으로 이벤트 호출을 지연시켜서 마지막에 발생한 이벤트만 동작처리 (debouncing)
+					//단시간에 많이 발생하는 이벤트시 함수 호출을 줄이는 방법
+					//debouncing: 이벤트 발생시 바로 호출하는게 아닌 일정시간 텀을 두고 마지막에 발생한 이벤트만 호출
+					//throttling: 이벤트 발생시 호출되는 함수자체를 setTimeout으로 적게 호출
+					enableEvent.current = true;
 				}
 			};
 		});
 	};
 
-	//흥미 (기본)
-	useEffect(() => getFlickr({ type: 'interest' }), []);
+	//기존 갤러리 초기화 함수
+	const resetGallery = (e) => {
+		const btns = btnSet.current.querySelectorAll('button');
+		btns.forEach((el) => el.classList.remove('on'));
+		e.target.classList.add('on');
+		enableEvent.current = false;
+		setLoader(true);
+		frame.current.classList.remove('on');
+	};
 
-	//미션 - 아래 호출문으로 풍경 이미지 검색되도록 함수 코드 수정
-	//useEffect(() => getFlickr({ type: 'search', tags: 'landscape' }), []);
+	const showInterest = (e) => {
+		//재이벤트, 모션중 재이벤트 방지
+		if (!enableEvent.current) return;
+		if (e.target.classList.contains('on')) return;
 
-	//미션2 - 아래 호출문으로 내 계정의 이미지 갤러리 호출 되도록
-	// useEffect(() => getFlickr({ type: 'user', user: '198489363@N07' }), []);
-	// useEffect(() => getFlickr({ type: 'user', user: '139000974@N08' }), []);
+		//기존 갤러리 초기화 함수 호출
+		resetGallery(e);
+
+		//새로운 데이터로 갤러리 생성 함수 호출
+		getFlickr({ type: 'interest' });
+	};
+
+	const showMine = (e) => {
+		//재이벤트, 모션중 재이벤트 방지
+		if (!enableEvent.current) return;
+		if (e.target.classList.contains('on')) return;
+
+		//기존 갤러리 초기화 함수 호출
+		resetGallery(e);
+
+		//새로운 데이터로 갤러리 생성 함수 호출
+		getFlickr({ type: 'user', user: '198489363@N07' });
+	};
+
+	const showSearch = (e) => {
+		const tag = searchInput.current.value.trim();
+		if (tag === '') return alert('검색어를 입력하세요.');
+		if (!enableEvent.current) return;
+
+		//기존 갤러리 초기화 함수 호출
+		resetGallery(e);
+
+		//새로운 데이터로 갤러리 생성 함수 호출
+		getFlickr({ type: 'search', tags: tag });
+	};
+
+	useEffect(() => getFlickr({ type: 'user', user: '198489363@N07' }), []);
 
 	return (
 		<Layout name={'Gallery'}>
+			<div className='btnSet' ref={btnSet}>
+				<button onClick={showInterest}>Interest Gallery</button>
+
+				<button className='on' onClick={showMine}>
+					My Gallery
+				</button>
+			</div>
+
+			<div className='searchBox'>
+				<input type='text' placeholder='검색어를 입력하세요.' ref={searchInput} onKeyPress={(e) => e.key === 'Enter' && showSearch(e)} />
+				<button onClick={showSearch}>Search</button>
+			</div>
+
 			<div className='frame' ref={frame}>
 				<Masonry elementType={'div'} options={{ transitionDuration: '0.5s' }}>
 					{Items.map((item, idx) => {
@@ -71,7 +138,15 @@ function Gallery() {
 											alt={item.owner}
 											onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
 										/>
-										<span>{item.owner}</span>
+										<span
+											onClick={(e) => {
+												setLoader(true);
+												frame.current.classList.remove('on');
+												getFlickr({ type: 'user', user: e.target.innerText });
+											}}
+										>
+											{item.owner}
+										</span>
 									</div>
 								</div>
 							</article>

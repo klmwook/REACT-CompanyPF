957d9b30af18195f31ba7d36733574fe40a0b9ad
klmwook <klmwook72@gmail.com>
2023-07-13 10:45:41 +0900

    리듀서 함수 생성

--- src/components/redux/reducer.js
+++ src/components/redux/reducer.js
@@ -1,1 +1,23 @@
 //전역 state에 데이터를 액션 타입의 종류에 따라서 변경한뒤 반환하는 함수
+import { combineReducers } from 'redux';
+import * as types from './actionType';
+
+const youtubeReducer = (state = { youtube: [] }, action) => {
+	switch (action.type) {
+		//컴포넌트로 부터 넘겨받는 action 객체
+		//해당 객체를 넘겨받으면 saga가 해당 타입에 대한 비동기 데이터 처리하고 새로운 객체 반환
+		case types.YOUTUBE.start:
+			return state;
+		//saga로 부터 새롭게 넘겨받은 action 객체로 데이터 처리 (데이터 fetching 성공시)
+		case types.YOUTUBE.success:
+			return { ...state, youtube: action.payload };
+		//saga로 부터 새롭게 넘겨받은 action 객체로 데이터 처리 (데이터 fetching 실패시)
+		case types.YOUTUBE.fail:
+			return { ...state, youtube: action.payload };
+		default:
+			return state;
+	}
+};
+
+const reducers = combineReducers({ youtubeReducer });
+export default reducers;

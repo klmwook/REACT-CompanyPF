e47e8164462ec8a041f2bb25e7275e6a4187b999
klmwook <klmwook72@gmail.com>
2023-07-14 12:53:30 +0900

    서브페이지 Gallery 새로고침 시 팝업 안뜨게 수정

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -6,13 +6,11 @@ import { useSelector, useDispatch } from 'react-redux/';
 import * as types from '../../redux/actionType';
 
 /*
-
 갤러리 컴포넌트에서 전역 비동기 데이터 변경 방법
 	dispatch를 액션 객체를 보낼때 opt도 같이 전달하면 됨
 
 	각각의 showInterest , showSearch , ShowMine , ShowUser 함수가 실행될 때 마다 내부적으로 opt 지역 State 변경
 	useEffec에 Opt 스테이트 값을 의존성 배열해서 dispatch로 opt가 바뀔때마다 action 객체를 전달하도록 설정
-
 */
 
 function Gallery() {
@@ -27,64 +25,17 @@ function Gallery() {
 	const frame = useRef(null);
 	const openModal = useRef(null);
 	const counter = useRef(0);
+
+	//처음 마운트되었는지 확인하가 위한 정보값
+	const firstLoaded = useRef(true);
+
 	const [Loader, setLoader] = useState(true);
 	const [Index, setIndex] = useState(0);
+
+	//초기 Opt State에 내 계정 정보 등록 : 해당 페이지 새로고침시 myGallery를 Default로 출력하기 위함
 	const [Opt, setOpt] = useState({ type: 'user', user: '198489363@N07' });
 
-	// const getFlickr = useCallback(
-	// 	async (opt) => {
-	// 		let counter = 0;
-
-	// 		// const result = await axios.get(url);
-	// 		// if (result.data.photos.photo.length === 0) {
-	// 		// 	setLoader(false);
-	// 		// 	frame.current.classList.add('on');
-	// 		// 	btnSearch.current.classList.remove('on');
-	// 		// 	const btnMine = btnSet.current.children;
-	// 		// 	btnMine[1].classList.add('on');
-	// 		// 	getFlickr({ type: 'user', user: '164021883@N04' });
-	// 		// 	enableEvent.current = true;
-
-	// 		// 	return alert('이미지 결과값이 없습니다.');
-	// 		// }
-
-	// 		//외부 API로 부터 데이터 fetching 시간이 오래 걸리는 경우
-	// 		//컴포넌트가 unMounted 시 해당 Mounted 값을 false로 변경처리
-	// 		//Mounted 값이 true일때에만 fetching된 데이터를 state에 담음
-	// 		//데이터가 fetching 전 컴포넌트가 언마운트되면 State에 값을 담지 않으므로 불필요한 메모리 누수가 발생하지 않음
-	// 		//Mounted && setItems(result.data.photos.photo);
-	// 		Mounted && setItems(result);
-
-	// 		const imgs = frame.current?.querySelectorAll('img');
-
-	// 		if (!imgs) return;
-
-	// 		imgs.forEach((img) => {
-	// 			img.onload = () => {
-	// 				++counter;
-
-	// 				//임시방편 - 전체 이미지 갯수가 하나 모잘라도 출력되게 수정
-	// 				//문제점 - myGallery, interestGallery는 전체 이미지 카운트가 잘 되는데 특정 사용자 갤러리만 갯수가 1씩 모자라는 현상
-	// 				// 검색 결과물에서 특정 사용자를 클릭하면 다시 결과값이 하나 적게 리턴되는 문제 (해결필요)
-	// 				if (counter === imgs.length - 2) {
-	// 					setLoader(false);
-	// 					frame.current.classList.add('on');
-
-	// 					//모션중 재이벤트 방지시 모션이 끝날때까지 이벤트를 방지를 시켜도
-	// 					//모션이 끝나는순간에도 이벤트가 많이 발생하면 특정값이 바뀌는 순간보다 이벤트가 더 빨리들어가서 오류가 발생가능
-	// 					//해결방법 - 물리적으로 이벤트 호출을 지연시켜서 마지막에 발생한 이벤트만 동작처리 (debouncing)
-	// 					//단시간에 많이 발생하는 이벤트시 함수 호출을 줄이는 방법
-	// 					//debouncing: 이벤트 발생시 바로 호출하는게 아닌 일정시간 텀을 두고 마지막에 발생한 이벤트만 호출
-	// 					//throttling: 이벤트 발생시 호출되는 함수자체를 setTimeout으로 적게 호출
-	// 					enableEvent.current = true;
-	// 				}
-	// 			};
-	// 		});
-	// 	},
-	// 	[Mounted]
-	// );
-
-	//기존 갤러리 초기화 함수
+	//이벤트 발생 시 각각 interest , mine , search , useGallery 호출 할 때마다 기존 데이터 사라지게 하고 loading보이게 하는 공통 초기화 함수
 	const resetGallery = (e) => {
 		const btns = btnSet.current.querySelectorAll('button');
 		btns.forEach((el) => el.classList.remove('on'));
@@ -94,6 +45,7 @@ function Gallery() {
 		frame.current.classList.remove('on');
 	};
 
+	//Interest 방식 갤러리 호출 함수
 	const showInterest = (e) => {
 		//재이벤트, 모션중 재이벤트 방지
 		if (!enableEvent.current) return;
@@ -103,10 +55,12 @@ function Gallery() {
 		resetGallery(e);
 
 		//새로운 데이터로 갤러리 생성 함수 호출
+		//Action 객체에 추가로 전달해야 될 옵션을 Opt State로 변경 처리
 		setOpt({ type: 'interest' });
 		isUser.current = false;
 	};
 
+	//MyGallery 호출 함수
 	const showMine = (e) => {
 		//재이벤트, 모션중 재이벤트 방지
 		if (!enableEvent.current) return;
@@ -120,6 +74,7 @@ function Gallery() {
 		isUser.current = false;
 	};
 
+	//검색 갤러리 호출 함수
 	const showSearch = (e) => {
 		const tag = searchInput.current.value.trim();
 		if (tag === '') return alert('검색어를 입력하세요.');
@@ -130,25 +85,29 @@ function Gallery() {
 		searchInput.current.value = '';
 	};
 
+	//액션에 추가로 전달되야될 Opt 값이 변경될 때 마다 새롭게 액션 객체를 생성해서 리듀서에 전달
 	useEffect(() => {
 		dispathch({ type: types.FLICKR.start, opt: Opt });
 	}, [Opt, dispathch]);
 
+	//전역 스테이트 정보값이 변경이 될 때 마다 해당 구문 실행
+	//다시 이벤트 기능 활성화, 이미지 로딩 이벤트 발생해서 이미지 소스 출력 완료 시 갤러리 보이게 처리, 버튼도 활성화
 	useEffect(() => {
 		console.log(Items);
 		counter.current = 0;
 
-		if (Items.length === 0) {
+		//처음 마운트가 된 상태가 아닐때에만 Items의 결과값이 없을때 경고창 출력
+		if (Items.length === 0 && !firstLoaded.current) {
 			setLoader(false);
 			frame.current.classList.add('on');
-			btnSearch.current.classList.remove('on');
 			const btnMine = btnSet.current.children;
 			btnMine[1].classList.add('on');
 			setOpt({ type: 'user', user: '164021883@N04' });
 			enableEvent.current = true;
-
 			return alert('이미지 결과값이 없습니다.');
 		}
+		//처음 마운트이후 firstLoaded.current값을 false로 변경
+		firstLoaded.current = false;
 
 		const imgs = frame.current?.querySelectorAll('img');
 

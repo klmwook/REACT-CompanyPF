330d77a00e2f85b35467533fe013582060d9916c
klmwook <klmwook72@gmail.com>
2023-07-28 09:51:14 +0900

    useQuery커스텀 hook

--- src/App.js
+++ src/App.js
@@ -22,16 +22,11 @@ import { fetchDepartment } from './redux/departmentSlice';
 import { fetchFlickr } from './redux/flickrSlice';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-
-/* 
-	redux-toolkit의 작업흐름
-	1. redux 폴더 안쪽에 작업하려는 data의 slice 파일 생성 (data fetching후 액션객체 생성 함수, 액션 객체 받아서 전역 데이터 수정 함수)
-	2. index.js에서 slice 값으로 연동된 데이터 store에 저장하고 App에 전달
-	3. app.js에서 slice파일로부터 action 객체 생성 함수를 import 후 호출하여 action 만들고 dispatch로 전달
-	4. 원하는 컴포넌트에서 useSelector로 데이터 가져오기
-*/
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools/build/lib/devtools';
 
 function App() {
+	const queryClient = new QueryClient();
 	const dispatch = useDispatch();
 
 	useEffect(() => {
@@ -41,8 +36,7 @@ function App() {
 	}, [dispatch]);
 
 	return (
-		<>
-			{/* Switch는 내부에 중복되는 라우트 경로가 있을 때 더 구체적인 라우터를 체크하고 나머지는 무시 */}
+		<QueryClientProvider client={queryClient}>
 			<Switch>
 				<Route exact path='/' render={() => <Main />} />
 				<Route path='/' render={() => <Header type={'sub'} />} />
@@ -56,7 +50,8 @@ function App() {
 			<Route path='/member' component={Member} />
 			<Footer />
 			<Menu />
-		</>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	);
 }
 

0e44a3862f9097f5970e057537c3c6ceddec59a3
klmwook <klmwook72@gmail.com>
2023-08-03 12:16:06 +0900

    무언가 바뀌었는데 모르겠음

--- src/App.js
+++ src/App.js
@@ -20,12 +20,33 @@ import './scss/style.scss';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 
 function App() {
+	console.log('rendered!!');
+	const [Count, setCount] = useState(0);
+	const [Count2, setCount2] = useState(0);
 	const queryClient = new QueryClient();
+
+	const returnPromise = () => {
+		return new Promise((res) => setTimeout(res, 500));
+	};
+
+	const handleClick = () => {
+		returnPromise().then(() => {
+			setCount(Count + 1);
+			setCount2(Count2 + 2);
+		});
+	};
 
 	return (
 		<QueryClientProvider client={queryClient}>
+			<div style={{ position: 'fixed', zIndex: 10 }}>
+				<button onClick={handleClick}>button</button>
+				<h1 style={{ color: '#fff' }}>
+					{Count} - {Count2}
+				</h1>
+			</div>
 			<Switch>
 				<Route exact path='/' render={() => <Main />} />
 				<Route path='/' render={() => <Header type={'sub'} />} />
